c86021d7b650c25e88edd8a0dde5b8ef9cb96d52
王刚 <wanggang@zxisl.com>
2014-12-12 15:28:43 +0800

    增加URL编码工具

--- src/main/java/zx/soft/utils/codec/URLCodecUtils.java
+++ src/main/java/zx/soft/utils/codec/URLCodecUtils.java
@@ -2,6 +2,7 @@ package zx.soft.utils.codec;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 /**
  * URL编解码器工具类
@@ -10,6 +11,15 @@ import java.net.URLDecoder;
  *
  */
 public class URLCodecUtils {
+
+	public static String encoder(String url, String codec) {
+		try {
+			String result = URLEncoder.encode(url, codec);
+			return result;
+		} catch (UnsupportedEncodingException e) {
+			return url;
+		}
+	}
 
 	public static String decoder(String url, String codec) {
 		try {
