518f9f6891924a782a695bb61f0abe9b3f493c6b
Dong lei <d21958@yeah.net>
2015-07-16 11:46:54 +0800

    修改Json版本为2.4.4

--- src/main/java/zx/soft/utils/json/JsonNodeUtils.java
+++ src/main/java/zx/soft/utils/json/JsonNodeUtils.java
@@ -3,12 +3,13 @@ package zx.soft.utils.json;
 import java.io.IOException;
 
 import org.apache.commons.lang.StringUtils;
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * JSON工具类，主要用于：
