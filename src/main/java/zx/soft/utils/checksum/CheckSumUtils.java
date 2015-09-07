4b8b3d24189a7fd17dbaadc60ff35376981ded6e
Dong lei <d21958@yeah.net>
2015-09-08 00:29:14 +0800

    修改MD5实现，并添加1.2.1中删除的reslet converter支持

--- src/main/java/zx/soft/utils/checksum/CheckSumUtils.java
+++ src/main/java/zx/soft/utils/checksum/CheckSumUtils.java
@@ -1,16 +1,19 @@
 package zx.soft.utils.checksum;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.zip.CRC32;
 import java.util.zip.Checksum;
 
 /**
  * 校验工具类
- * 
+ *
  * @author wanggang
  *
  */
 public class CheckSumUtils {
+
+	public static final int MD5_LENGTH = 16; // bytes
 
 	/**
 	 * CRC32校验
@@ -25,35 +28,39 @@ public class CheckSumUtils {
 		return checksum.getValue();
 	}
 
-	/***
+	/**
+	 * modified by donglei
 	 * MD5加码 生成32位md5码
 	 */
 	public static String getMD5(String str) {
 
-		MessageDigest md5 = null;
-		try {
-			md5 = MessageDigest.getInstance("MD5");
-		} catch (Exception e) {
-			return null;
-		}
-		if (md5 == null) {
-			return null;
-		}
-		char[] charArray = str.toCharArray();
-		byte[] byteArray = new byte[charArray.length];
-
-		for (int i = 0; i < charArray.length; i++)
-			byteArray[i] = (byte) charArray[i];
-		byte[] md5Bytes = md5.digest(byteArray);
+		byte[] md5Bytes = md5sum(str);
 		StringBuffer hexValue = new StringBuffer();
 		for (int i = 0; i < md5Bytes.length; i++) {
 			int val = (md5Bytes[i]) & 0xff;
-			if (val < 16)
+			if (val < 16) {
 				hexValue.append("0");
+			}
 			hexValue.append(Integer.toHexString(val));
 		}
 
 		return hexValue.toString();
+	}
+
+	/**
+	 * @author donglei
+	 * @param str
+	 * @return
+	 */
+	public static byte[] md5sum(String str) {
+		MessageDigest d;
+		try {
+			d = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("MD5 algorithm not available!", e);
+		}
+
+		return d.digest(str.getBytes());
 	}
 
 }
--- src/test/java/zx/soft/utils/checksum/CheckSumUtilsTest.java
+++ src/test/java/zx/soft/utils/checksum/CheckSumUtilsTest.java
@@ -7,14 +7,54 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
-import zx.soft.utils.checksum.CheckSumUtils;
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CheckSumUtilsTest {
 
 	@Test
-	public void testString2MD5() {
+	public void testString2MD57() {
+		assertEquals("d174ab98d277d9f5a5611c2c9f419d9f",
+				CheckSumUtils.getMD5("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"));
+	}
+
+	@Test
+	public void testString2MD58() {
+		assertEquals("57edf4a22be3c955ac49da2e2107b67a",
+				CheckSumUtils
+						.getMD5("12345678901234567890123456789012345678901234567890123456789012345678901234567890"));
+	}
+
+	@Test
+	public void testString2MD51() {
 		assertEquals("a906449d5769fa7361d7ecc6aa3f6d28", CheckSumUtils.getMD5("123abc"));
+	}
+
+	@Test
+	public void testString2MD52() {
+		assertEquals("d41d8cd98f00b204e9800998ecf8427e", CheckSumUtils.getMD5(""));
+		assertEquals(CheckSumUtils.MD5_LENGTH, CheckSumUtils.md5sum("").length);
+	}
+
+	@Test
+	public void testString2MD53() {
+		assertEquals("0cc175b9c0f1b6a831c399e269772661", CheckSumUtils.getMD5("a"));
+		assertEquals(CheckSumUtils.MD5_LENGTH, CheckSumUtils.md5sum("a").length);
+	}
+
+	@Test
+	public void testString2MD54() {
+		assertEquals("900150983cd24fb0d6963f7d28e17f72", CheckSumUtils.getMD5("abc"));
+		assertEquals(CheckSumUtils.MD5_LENGTH, CheckSumUtils.md5sum("abc").length);
+
+	}
+
+	@Test
+	public void testString2MD55() {
+		assertEquals("f96b697d7cb7938d525a2f31aaf161d0", CheckSumUtils.getMD5("message digest"));
+	}
+
+	@Test
+	public void testString2MD56() {
+		assertEquals("c3fcd3d76192e4007dfb496cca67e13b", CheckSumUtils.getMD5("abcdefghijklmnopqrstuvwxyz"));
 	}
 
 	@Test
